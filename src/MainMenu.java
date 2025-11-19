a8ceb6f46507cf12f3f3bba0c2f66157158c91df
Desirée Gunnarsson <dezzie@live.se>
2025-11-19 11:05:31 +0100

    Corrected menu text

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,16 +1,14 @@
 
 public enum MainMenu {
-    OPTION_ONE(1, "Add transaction"),
-    OPTION_TWO(2, "Show account balance"),
-    OPTION_THREE(3, "Delete a transaction"),
-    OPTION_FOUR(4, "Display expenses"),
-    OPTION_FIVE(5, "Display income"),
-    OPTION_SIX(6,"Exit");
+    OPTION_ONE(1,"Take a quiz"),
+    OPTION_TWO(2,"Make a quiz"),
+    OPTION_THREE(3, "Your score"),
+    OPTION_FOUR(4,"Quit the game");
 
     private final int number;
     private final String description;
 
-    MainMenu (int number, String description){
+    MainMenu(int number, String description) {
         this.number = number;
         this.description = description;
     }
@@ -18,12 +16,10 @@ public enum MainMenu {
     public void print() {
         System.out.println(number + ". " + description);
     }
-
     public static void printMenu() {
-        System.out.println();
-        System.out.println("--- MENU ---");
-        for (MainMenu option : MainMenu.values()) {
-            option.print();
+        System.out.println("~~~ QUIZ MENU ~~~");
+        for (MainMenu menu : MainMenu.values()) {
+            menu.print();
         }
     }
 }
